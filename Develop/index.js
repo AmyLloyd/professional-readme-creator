13b625abebc242fa8fa5afee3dee87178d94f643
Create-88 <141834742+Create-88@users.noreply.github.com>
2023-11-07 15:26:00 +1030

    init and install inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -3,13 +3,34 @@ const fs = require('fs');
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your name?", "What is your gitHub username?", "Which languages are you proficient in?", "What are your strengths as a developer?", "What are your strengths as a team member?", "What do you want to achieve in your work?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init(questions) {
+    inquirer
+    .createPromptModule([
+        for (i = 0; i < questions.length; i++) {
+            type: "input",
+            message: `${questions[i]}`
+            name: 
+        }
+        {
+            type:"input",
+            message: questions,
+            name: "name",
+        },
+
+    ])
+    .then((response)) => {
+        let response = JSON.stringify(response);
+        console.log(response);
+
+
+    }
+}
 
 // Function call to initialize app
 init();
